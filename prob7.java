63cd69adb539797e677879b0b6ce577f8a812fe5
debasish2050 <41298439+debasish2050@users.noreply.github.com>
2019-08-04 11:35:07 +0530

    Update prob7.java

--- prob7.java
+++ prob7.java
@@ -4,10 +4,10 @@ public class prob7 {
 
 	public static void main(String[] args) {
 		int flag,n=10001,c=0;
-		for(long i=2;i<=1000000;i++)
+		for(long i=3;i<=1000000;i+=2)
 		{
 			flag=0;
-			for(long j=2;j<=i/2;j++)
+			for(long j=2;j<=Math.sqrt(i);j++)
 			{
 				if(i%j==0)
 				{
